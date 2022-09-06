193504d4f8f673fba18545c5c30ba8418999c38b
Daniel Ng <m.daniel.ng1@gmail.com>
2022-09-05 22:08:50 -0400

    Add files via upload

---
+++ MinStack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MinStack
+{
+    public class MinStack
+    {
+        public MinStack() => Stack = new Stack<int>();
+
+        public Stack<int> Stack { get; set; }
+
+        public void GetMin()
+        {
+            if (Stack.Count == 0)
+            {
+                Console.WriteLine("Stack is empty.");
+            }
+            else
+            {
+                Console.WriteLine($"Minimum element in the stack is {Stack.Min()}");
+            }
+        }
+
+        public void Pop() => Stack.Pop();
+
+        public void Push(int val) => Stack.Push(val);
+        
+        public void Top()
+        {
+            if (Stack.Count == 0)
+            {
+                Console.WriteLine("Stack is empty.");
+            }
+            else
+            {
+                Console.WriteLine($"Top element in the stack is {Stack.LastOrDefault()}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MinStack
+{
+    class Program
+    {
+        static void Main()
+        {
+            MinStack minStack = new MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            minStack.GetMin();
+            minStack.Pop();
+            minStack.Top();
+            minStack.GetMin();
+        }
+    }
+}
